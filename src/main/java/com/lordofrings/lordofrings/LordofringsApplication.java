9cf70db05ddf3d819705f24addec9436e6195206
sindhuja akula <sindhuja9akula@gmail.com>
2023-03-09 10:54:02 -0700

    first commit

---
+++ src/main/java/com/lordofrings/lordofrings/LordofringsApplication.java
@@ -0,0 +1,13 @@
+package com.lordofrings.lordofrings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordofringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordofringsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lordofrings/lordofrings/LordofringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lordofrings.lordofrings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LordofringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
