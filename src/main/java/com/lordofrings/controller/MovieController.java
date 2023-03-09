71496f50403c7481994f6c1a3d465102dd1a169a
sindhujaakula09 <sindhuja9akula@gmail.com>
2023-03-09 12:17:02 -0700

    refactored code

--- src/main/java/com/lordofrings/controller/MovieController.java
+++ src/main/java/com/lordofrings/controller/MovieController.java
@@ -1,9 +1,12 @@
 package com.lordofrings.controller;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,34 +15,44 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MovieController {
+	private static final String RESOURCE_URL = "https://the-one-api.dev/v2";
 	private HttpHeaders headers = new HttpHeaders();
-	private HttpEntity<String> entity = new HttpEntity<String>(headers);
-	private final String resouceUrl = "https://the-one-api.dev/v2/";
-	
-	private MovieController() {
-		headers.set("Authorization", "Bearer OfCZLOecM9CTcdwX7kpU");
-	}
-    
-
+	HttpEntity<String> entity = new HttpEntity<>(headers);
 	@Autowired
 	RestTemplate restTemplate;
 
-	// ************GET APIs**************//
+	public MovieController() {
 
-	@GetMapping("/movie")
-	public ResponseEntity<String> getAllMovies() {
-		return restTemplate.exchange(resouceUrl.concat("movie"), HttpMethod.GET, entity, String.class);        
-        
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.set("Authorization", "Bearer OfCZLOecM9CTcdwX7kpU");
+
 	}
-	
-	@GetMapping("/movie/{id}") 
-	public ResponseEntity<String> getMovieById(@PathVariable final String id){
-		return restTemplate.exchange(resouceUrl.concat("movie/").concat(id), HttpMethod.GET, entity, String.class); 
+
+	@GetMapping(path = "/movie", produces = "application/json")
+	public String getMovies() {
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(RESOURCE_URL.concat("/movie"), HttpMethod.GET,
+				entity, String.class);
+
+		return responseEntity.getBody();
 	}
-	
-	@GetMapping("/movie/{id}/quote") 
-	public ResponseEntity<String> getMovieQuoteById(@PathVariable final String id){
-		return restTemplate.exchange(resouceUrl.concat("movie/").concat(id).concat("/quote"), HttpMethod.GET, entity, String.class); 
+
+	@GetMapping(path = "/movie/{id}", produces = "application/json")
+	public String getMoviesById(@PathVariable final String id) {
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(RESOURCE_URL.concat("/movie/").concat(id),
+				HttpMethod.GET, entity, String.class);
+
+		return responseEntity.getBody();
 	}
-	
+
+	@GetMapping(path = "/movie/{id}/quote", produces = "application/json")
+	public String getMoviesByIdQuote(@PathVariable final String id) {
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(
+				RESOURCE_URL.concat("/movie/").concat(id).concat("/quote"), HttpMethod.GET, entity, String.class);
+
+		return responseEntity.getBody();
+	}
+
 }
